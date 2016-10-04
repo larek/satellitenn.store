a35574495071eb2996ea3912545e5a58fe1c9b1a
Alexandr Tikhonov <t9101029991@gmail.com>
2016-10-04 23:52:34 +0300

    style items

--- basic/views/site/_itemCategory.php
+++ basic/views/site/_itemCategory.php
@@ -6,7 +6,9 @@ use yii\helpers\Html;
     <div class="thumbnail">
       <?= Html::img('/uploads/300x200/'.$model->image, ['class' => 'img img-responsive']);?>
       <div class="caption">
-        <h5><?= Html::a($model->title,['site/category', 'id' => $model->url])?></h5>
+      	<div class="item_title">
+        	<h5><?= Html::a($model->title,['site/category', 'id' => $model->url])?></h5>
+        </div>
       </div>
     </div>
   </div>
--- basic/views/site/_itemProduct.php
+++ basic/views/site/_itemProduct.php
@@ -6,7 +6,9 @@ use yii\helpers\Html;
     <div class="thumbnail">
       <?= Html::img('/uploads/300x200/'.$model->photo, ['class' => 'img img-responsive']);?>
       <div class="caption">
-        <h5><?= Html::a($model->title,['site/product', 'id' => $model->url])?></h5>
+      	<div class="item_title">
+        	<h5><?= Html::a($model->title,['site/product', 'id' => $model->url])?></h5>
+        </div>
         <p><?= $model->price?></p>
         <span class="btn btn-default btn-add" id='<?= $model->id?>'>В корзину</span>
       </div>
