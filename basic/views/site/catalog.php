1e9923e7423d3d17609777b2aaf1414bd9b6eab3
Alexandr Tikhonov <t9101029991@gmail.com>
2016-10-20 16:08:19 +0300

    catalog title and breadcrumbs

--- basic/views/site/catalog.php
+++ basic/views/site/catalog.php
@@ -1,32 +1,27 @@
 <?php
-use yii\widgets\ListView;
+
 /* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\ContactForm */
 
-$this->title = 'Каталог Satellitenn Store';
+use yii\helpers\Html;
+use yii\widgets\Breadcrumbs;
+use yii\widgets\ListView;
+
+
+$this->title = 'Каталог';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-
-
-
-
-        <div class="content content-margin">
-
-        <br><br>
-        <div class="row">
-        
-      		<?= ListView::widget([
-      			'dataProvider' => $dataProvider,
-      			'itemView' => '_itemCategory',
-                'summary' => false
-      		]);?>
-        
-		</div>
-        
-        
-        
-
-            
-              
-        </div>
-
-    
-    
+<div class="content content-margin">
+            <h1><?= $this->title?></h1>
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <div class="row">
+              <?= ListView::widget([
+              'dataProvider' => $dataProvider,
+              'itemView' => '_itemCategory',
+                  'summary' => false
+              ]);?>
+            </div>
+</div>
